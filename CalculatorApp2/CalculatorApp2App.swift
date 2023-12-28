a61b4ac9b542f1f73bd2bb8281469c331036ebc1
Taimoor Arif <tamoorarif20@gmail.com>
2023-12-28 14:08:00 +0500

    first commit

---
+++ CalculatorApp2/CalculatorApp2App.swift
@@ -0,0 +1,19 @@
+//
+//  CalculatorApp2App.swift
+//  CalculatorApp2
+//
+//  Created by Usama Nayyer on 29/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp2App: App {
+    var body: some Scene {
+            WindowGroup {
+                NavigationStack{
+                ContentView()
+            }
+        }
+    }
+}
